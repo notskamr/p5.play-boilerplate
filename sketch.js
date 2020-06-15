b98d095ea239ec2f957830f12f3c4916ad8286d0
notskamr <66314478+notskamr@users.noreply.github.com>
2020-06-15 17:30:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,28 @@
+var shape1, shape2;
+function preload() {
+
+
+}
+
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  shape1 = createSprite(300, 200, 50, 50);
+  shape2 = createSprite(100, 200, 50, 50);
+  shape1.velocityX = -2;
+  shape2.velocityX = 2;
+
 }
 
 function draw() {
   background(255,255,255);  
   drawSprites();
+}
+
+function collide() {
+if(shape1.x + 50 == shape2.x + 50) {
+shape1.color = 'red';
+
+
+}
 }
